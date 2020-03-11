938f2304a7da69d2a7805c30855140a60af21560
yujingbo <1935952410@qq.com>
2020-03-11 16:35:03 +0800

    xx

--- 04-jq/淘宝网/js/index.js
+++ 04-jq/淘宝网/js/index.js
@@ -1,4 +1,8 @@
+// 仅仅是一个入口函数  不能少  因为要在入口函数中操作DOM
 $(function(){
-	
-
+    // hover 鼠标移到menu上面就触发hover事件  top区域的hover效果
+	$("#user-nav li.menu").hover(function(){
+        // toggleClass表示切换一个类
+        $(this).toggleClass("active")
+    })
 });
