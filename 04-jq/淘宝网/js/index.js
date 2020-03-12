87503f49eca6e260ddf199a20b5eda09b1cece6b
yujingbo <1935952410@qq.com>
2020-03-12 11:40:15 +0800

    xx

--- 04-jq/淘宝网/js/index.js
+++ 04-jq/淘宝网/js/index.js
@@ -5,4 +5,31 @@ $(function(){
         // toggleClass表示切换一个类
         $(this).toggleClass("active")
     })
+
+    // ============== 控制选择宝贝或天猫或店铺
+    // $search_box  如果一个变量以$开头 表示一个JQ对象
+    // 后面要多次使用这个对象  使用一个变量来引用它
+    var $search_box = $("#search-box");
+    $search_box.find(".search-tab li").click(function(){
+        // hasClass 判断元素上面有没有类  如果有返回true
+        if($(this).hasClass("tmall")){
+            // console.log("点击了天猫")
+            $search_box.addClass("tianmao");
+        }else{
+            $search_box.removeClass("tianmao");
+        }
+        if($(this).hasClass("baobei")){
+            $search_box.addClass("tb");
+        }else{
+            $search_box.removeClass("tb");
+        }
+        if($(this).hasClass("dianpu")){
+            $search_box.addClass("dianpu");
+        }else{
+            $search_box.removeClass("dianpu");
+        }
+
+        // 点击了哪个li，需要给它上面添加on这个类
+        $(this).addClass("on").siblings().removeClass("on");
+    })
 });
