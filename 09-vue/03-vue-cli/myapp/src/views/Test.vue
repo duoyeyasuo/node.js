d16a376085c5f11a14a1cad0f732fd4f42024f76
yujingbo <1935952410@qq.com>
2020-04-15 16:41:21 +0800

    xx

---
+++ 09-vue/03-vue-cli/myapp/src/views/Test.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Test组件</h1>
+        <p>{{msg}}</p>
+        <ul>
+            <li v-for="(item,index) in news" :key="index">{{item}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            msg:"wangcai",
+            news:["new1","new2","new3"]
+        }
+    }
+}
+</script>
+<style scoped lang="less">
+    @color:pink;
+    h1{
+        color: @color;
+    }
+</style>
