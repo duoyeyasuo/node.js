8caa894aee05dd17117eaea54ce9066606ef97c4
yujingbo <1935952410@qq.com>
2020-04-21 17:07:51 +0800

    xx

---
+++ 09-vue/06-vue小练习/elmDemo步骤/04-手机号登录完成/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+     <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+#app {
+  width: 100%;
+  height: 100%;
+  font-size: 14px;
+  background: #f1f1f1;
+}
+</style>
---
+++ 09-vue/06-vue小练习/elmDemo步骤/04-手机号登录完成/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component: ()=>import("../views/Index.vue"),
+    children:[
+      { path:"",redirect: "/home" },
+      { path:"/home",component: ()=>import("../views/Home.vue"),name:"home" },
+      { path:"/order",component: ()=>import("../views/Order.vue"),name:"order" },
+      { path:"/me",component: ()=>import("../views/Me.vue"),name:"me" },
+    ]
+  },{
+    path: '/login',
+    name: 'index',
+    component: ()=>import("../views/Login.vue")
+  }
+]
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+// 全局路由守卫  下面判断不够严谨
+router.beforeEach((to, from, next) => {
+    let islogin = localStorage.ele_login ? true : false;
+    if(to.path == "/login"){
+      next()
+    }else{
+      // 看一下，你是否登录了
+      islogin ? next() : next("/login")
+    }
+})
+
+export default router
